3e7fec43fa7b3319497ca9ccb9b7d3406886db1e
fenggeren <huanao@huanaodemacbook-pro.local>
2019-01-03 10:46:50 +0800

    Initial Commit

---
+++ MyFuture/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyFuture
+//
+//  Created by huanao on 2019/1/3.
+//  Copyright © 2019年 fgr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
